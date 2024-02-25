d3988f58937e6252cd9cb5e35900e249d232af3c
Andy Babic <andy.babic@torchbox.com>
2024-02-25 15:45:05 +0000

    Improve test DRYness

--- tests/test_management_commands.py
+++ tests/test_management_commands.py
@@ -1,16 +1,14 @@
 import datetime
 
 from io import StringIO
+from typing import Type
 from unittest import mock
 
 from django.core.management import call_command
 from django.test import SimpleTestCase, TestCase
 from freezegun import freeze_time
 from testapp.factories import CustomDocumentFactory, CustomImageFactory, VideoFactory
-from wagtail.documents import get_document_model
-from wagtail.images import get_image_model
 
-from wagtail_bynder import get_video_model
 from wagtail_bynder.management.commands.update_stale_documents import (
     Command as UpdateStaleDocuments,
 )
@@ -20,6 +18,7 @@ from wagtail_bynder.management.commands.update_stale_images import (
 from wagtail_bynder.management.commands.update_stale_videos import (
     Command as UpdateStaleVideos,
 )
+from wagtail_bynder.models import BynderAssetMixin
 
 from .utils import TEST_ASSET_ID, get_test_asset_data
 
@@ -27,22 +26,32 @@ from .utils import TEST_ASSET_ID, get_test_asset_data
 TEST_ASSET_DATA = get_test_asset_data(id=TEST_ASSET_ID)
 
 
-class BaseSyncCommandTestCase(SimpleTestCase):
+class SyncCommandTestsMixin:
     """
-    A base class for testing 'update_stale_images', 'update_stale_documents' and
+    A mixin class for testing 'update_stale_images', 'update_stale_documents' and
     'update_stale_videos' commands, which uses mocking to patch out interactions
     with the Bynder API and the database.
     """
 
     command_name: str = ""
-    model_class = None
+    command_class: Type = None
+    uses_media_info_for_individual_assets: bool = False
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.model_class = cls.command_class.model
+        cls.asset_type = cls.command_class.bynder_asset_type
 
     def setUp(self) -> None:
         super().setUp()
+
+        # Define a mock to stand-in for the Bynder API client
         self.mock_api_client = mock.Mock()
         self.mock_api_client.asset_bank_client.media_list.return_value = [
             TEST_ASSET_DATA
         ]
+        self.mock_api_client.asset_bank_client.media_info.return_value = TEST_ASSET_DATA
 
         # Create an in-memory instance of the target model class, so that
         # the command is acting on a 'real' object
@@ -94,318 +103,138 @@ class BaseSyncCommandTestCase(SimpleTestCase):
             )
         return out.getvalue()
 
+    def assertCommandRunOutcome(
+        self,
+        output: StringIO,
+        mocked_client: mock.Mock,
+        model_instance: BynderAssetMixin,
+        expected_datemodified: datetime.datetime,
+        expected_timespan_description: str,
+    ):
+        """
+        Assert that the command output indicates success, and that the mocked
+        API client and `get_stale_objects` method were called as expected.
+        """
+        self.assertIn(
+            f"Looking for {self.asset_type} assets modified within the last {expected_timespan_description}",
+            output,
+        )
+        self.assertIn("Processing batch 1 (1 assets)...", output)
+        self.assertIn("1 stale objects were found for this batch", output)
+        self.assertIn(f"Updating object for asset '{TEST_ASSET_ID}'", output)
 
-@freeze_time()
-class UpdateStaleImagesTestCase(BaseSyncCommandTestCase):
+        # Test that 'media_list' was called as expected
+        mocked_client.asset_bank_client.media_list.assert_called_once_with(
+            {
+                "dateModified": expected_datemodified.strftime("%Y-%m-%dT%H:%M:%SZ"),
+                "orderBy": "dateModified desc",
+                "page": 1,
+                "limit": self.command_class.page_size,
+                "type": self.asset_type,
+            }
+        )
+
+        # Conditionally test that 'media_info' was called as expected
+        if self.uses_media_info_for_individual_assets:
+            mocked_client.asset_bank_client.media_info.assert_called_once_with(
+                TEST_ASSET_ID
+            )
+
+        # Check the patched object was updated and saved as expected
+        model_instance.update_from_asset_data.assert_called_once_with(TEST_ASSET_DATA)
+        model_instance.save.assert_called_once()
+
+    @freeze_time()
+    def test_default(self):
+        expected_datemodified = datetime.datetime.utcnow() - datetime.timedelta(days=1)
+        expected_timespan_description = "1 day"
+        output = self.call_command()
+
+        self.assertCommandRunOutcome(
+            output,
+            self.mock_api_client,
+            self.patched_obj,
+            expected_datemodified,
+            expected_timespan_description,
+        )
+
+    @freeze_time()
+    def test_minutes(self):
+        expected_datemodified = datetime.datetime.utcnow() - datetime.timedelta(
+            minutes=30
+        )
+        expected_timespan_description = "30 minute(s)"
+        output = self.call_command(minutes=30)
+
+        self.assertCommandRunOutcome(
+            output,
+            self.mock_api_client,
+            self.patched_obj,
+            expected_datemodified,
+            expected_timespan_description,
+        )
+
+    @freeze_time()
+    def test_hours(self):
+        expected_datemodified = datetime.datetime.utcnow() - datetime.timedelta(hours=3)
+        expected_timespan_description = "3 hour(s)"
+
+        # NOTE: 'minutes' should be ignored when hours is provided
+        output = self.call_command(hours=3, minutes=99999)
+
+        self.assertCommandRunOutcome(
+            output,
+            self.mock_api_client,
+            self.patched_obj,
+            expected_datemodified,
+            expected_timespan_description,
+        )
+
+    @freeze_time()
+    def test_days(self):
+        expected_datemodified = datetime.datetime.utcnow() - datetime.timedelta(days=3)
+        expected_timespan_description = "3 day(s)"
+
+        # NOTE: 'hours' and 'minutes' should be ignored when days is provided
+        output = self.call_command(days=3, hours=99999, minutes=99999)
+
+        self.assertCommandRunOutcome(
+            output,
+            self.mock_api_client,
+            self.patched_obj,
+            expected_datemodified,
+            expected_timespan_description,
+        )
+
+
+class UpdateStaleImagesTestCase(SyncCommandTestsMixin, SimpleTestCase):
     """
     Unit tests for the 'update_stale_images' management command.
     """
 
     command_name = "update_stale_images"
-    model_class = get_image_model()
-    common_api_kwargs = {
-        "orderBy": "dateModified desc",
-        "page": 1,
-        "limit": 200,
-        "type": "image",
-    }
-
-    def setUp(self) -> None:
-        super().setUp()
-        # NOTE: update_stale_images overrides update_object() to fetch the
-        # full asset details to pass to the super() implementation, therefore
-        # we need to mock the media_info() method too
-        self.mock_api_client.asset_bank_client.media_info.return_value = TEST_ASSET_DATA
-
-    def test_default(self):
-        expected_datemodified = datetime.datetime.utcnow() - datetime.timedelta(days=1)
-        output = self.call_command()
-
-        # Check the command output
-        self.assertIn("Looking for image assets modified in the last 1 day(s)", output)
-
-        # Check the mocked API client was called as expected
-        self.mock_api_client.asset_bank_client.media_list.assert_called_once_with(
-            {
-                "dateModified": expected_datemodified.strftime("%Y-%m-%dT%H:%M:%SZ"),
-                **self.common_api_kwargs,
-            }
-        )
-        self.mock_api_client.asset_bank_client.media_info.assert_called_once_with(
-            TEST_ASSET_ID
-        )
-
-        # Check the patched object was updated and saved as expected
-        self.patched_obj.update_from_asset_data.assert_called_once_with(TEST_ASSET_DATA)
-        self.patched_obj.save.assert_called_once()
-
-    def test_minutes(self):
-        expected_datemodified = datetime.datetime.utcnow() - datetime.timedelta(
-            minutes=30
-        )
-        output = self.call_command(minutes=30)
-
-        # Check the command output
-        self.assertIn(
-            "Looking for image assets modified in the last 30 minute(s)", output
-        )
-
-        # Check the mocked API client was called as expected
-        self.mock_api_client.asset_bank_client.media_list.assert_called_once_with(
-            {
-                "dateModified": expected_datemodified.strftime("%Y-%m-%dT%H:%M:%SZ"),
-                **self.common_api_kwargs,
-            }
-        )
-        self.mock_api_client.asset_bank_client.media_info.assert_called_once_with(
-            TEST_ASSET_ID
-        )
-
-        # Check the patched object was updated and saved as expected
-        self.patched_obj.update_from_asset_data.assert_called_once_with(TEST_ASSET_DATA)
-        self.patched_obj.save.assert_called_once()
-
-    def test_hours(self):
-        expected_datemodified = datetime.datetime.utcnow() - datetime.timedelta(hours=3)
-        output = self.call_command(hours=3)
-
-        # Check the command output
-        self.assertIn("Looking for image assets modified in the last 3 hour(s)", output)
-
-        # Check the mocked API was called as expected
-        self.mock_api_client.asset_bank_client.media_list.assert_called_once_with(
-            {
-                "dateModified": expected_datemodified.strftime("%Y-%m-%dT%H:%M:%SZ"),
-                **self.common_api_kwargs,
-            }
-        )
-        self.mock_api_client.asset_bank_client.media_info.assert_called_once_with(
-            TEST_ASSET_ID
-        )
-
-        # Check the patched object was updated and saved as expected
-        self.patched_obj.update_from_asset_data.assert_called_once_with(TEST_ASSET_DATA)
-        self.patched_obj.save.assert_called_once()
-
-    def test_days(self):
-        expected_datemodified = datetime.datetime.utcnow() - datetime.timedelta(days=3)
-        output = self.call_command(days=3)
-
-        # Check the command output
-        self.assertIn("Looking for image assets modified in the last 3 day(s)", output)
-
-        # Check the mocked API was called as expected
-        self.mock_api_client.asset_bank_client.media_list.assert_called_once_with(
-            {
-                "dateModified": expected_datemodified.strftime("%Y-%m-%dT%H:%M:%SZ"),
-                **self.common_api_kwargs,
-            }
-        )
-        self.mock_api_client.asset_bank_client.media_info.assert_called_once_with(
-            TEST_ASSET_ID
-        )
-
-        # Check the patched object was updated and saved as expected
-        self.patched_obj.update_from_asset_data.assert_called_once_with(TEST_ASSET_DATA)
-        self.patched_obj.save.assert_called_once()
+    command_class = UpdateStaleImages
+    uses_media_info_for_individual_assets = True
 
 
-@freeze_time()
-class UpdateStaleDocumentsTestCase(BaseSyncCommandTestCase):
+class UpdateStaleDocumentsTestCase(SyncCommandTestsMixin, SimpleTestCase):
     """
     Unit tests for the 'update_stale_documents' management command.
     """
 
     command_name = "update_stale_documents"
-    model_class = get_document_model()
-    common_api_kwargs = {
-        "orderBy": "dateModified desc",
-        "page": 1,
-        "limit": 200,
-        "type": "document",
-    }
-
-    def test_default(self):
-        expected_datemodified = datetime.datetime.utcnow() - datetime.timedelta(days=1)
-        output = self.call_command()
-
-        # Check the command output
-        self.assertIn(
-            "Looking for document assets modified in the last 1 day(s)", output
-        )
-
-        # Check the mocked API was called as expected
-        self.mock_api_client.asset_bank_client.media_list.assert_called_once_with(
-            {
-                "dateModified": expected_datemodified.strftime("%Y-%m-%dT%H:%M:%SZ"),
-                **self.common_api_kwargs,
-            }
-        )
-
-        # Check the patched object was updated and saved as expected
-        self.patched_obj.update_from_asset_data.assert_called_once_with(TEST_ASSET_DATA)
-        self.patched_obj.save.assert_called_once()
-
-    def test_minutes(self):
-        expected_datemodified = datetime.datetime.utcnow() - datetime.timedelta(
-            minutes=30
-        )
-        output = self.call_command(minutes=30)
-
-        # Check the command output
-        self.assertIn(
-            "Looking for document assets modified in the last 30 minute(s)", output
-        )
-
-        # Check the mocked API was called as expected
-        self.mock_api_client.asset_bank_client.media_list.assert_called_once_with(
-            {
-                "dateModified": expected_datemodified.strftime("%Y-%m-%dT%H:%M:%SZ"),
-                **self.common_api_kwargs,
-            }
-        )
-
-        # Check the patched object was updated and saved as expected
-        self.patched_obj.update_from_asset_data.assert_called_once_with(TEST_ASSET_DATA)
-        self.patched_obj.save.assert_called_once()
-
-    def test_hours(self):
-        expected_datemodified = datetime.datetime.utcnow() - datetime.timedelta(hours=3)
-        output = self.call_command(hours=3)
-
-        # Check the command output
-        self.assertIn(
-            "Looking for document assets modified in the last 3 hour(s)", output
-        )
-
-        # Check the mocked API was called as expected
-        self.mock_api_client.asset_bank_client.media_list.assert_called_once_with(
-            {
-                "dateModified": expected_datemodified.strftime("%Y-%m-%dT%H:%M:%SZ"),
-                **self.common_api_kwargs,
-            }
-        )
-
-        # Check the patched object was updated and saved as expected
-        self.patched_obj.update_from_asset_data.assert_called_once_with(TEST_ASSET_DATA)
-        self.patched_obj.save.assert_called_once()
-
-    def test_days(self):
-        expected_datemodified = datetime.datetime.utcnow() - datetime.timedelta(days=3)
-        output = self.call_command(days=3)
-
-        # Check the command output
-        self.assertIn(
-            "Looking for document assets modified in the last 3 day(s)", output
-        )
-
-        # Check the mocked API was called as expected
-        self.mock_api_client.asset_bank_client.media_list.assert_called_once_with(
-            {
-                "dateModified": expected_datemodified.strftime("%Y-%m-%dT%H:%M:%SZ"),
-                **self.common_api_kwargs,
-            }
-        )
-
-        # Check the patched object was updated and saved as expected
-        self.patched_obj.update_from_asset_data.assert_called_once_with(TEST_ASSET_DATA)
-        self.patched_obj.save.assert_called_once()
+    command_class = UpdateStaleDocuments
+    uses_media_info_for_individual_assets = False
 
 
-@freeze_time()
-class UpdateStaleVideosTestCase(BaseSyncCommandTestCase):
+class UpdateStaleVideosTestCase(SyncCommandTestsMixin, SimpleTestCase):
     """
     Unit tests for the 'update_stale_videos' management command.
     """
 
     command_name = "update_stale_videos"
-    model_class = get_video_model()
-    common_api_kwargs = {
-        "orderBy": "dateModified desc",
-        "page": 1,
-        "limit": 200,
-        "type": "video",
-    }
-
-    def test_default(self):
-        expected_datemodified = datetime.datetime.utcnow() - datetime.timedelta(days=1)
-        output = self.call_command()
-
-        # Check the command output
-        self.assertIn("Looking for video assets modified in the last 1 day(s)", output)
-
-        # Check the mocked API was called as expected
-        self.mock_api_client.asset_bank_client.media_list.assert_called_once_with(
-            {
-                "dateModified": expected_datemodified.strftime("%Y-%m-%dT%H:%M:%SZ"),
-                **self.common_api_kwargs,
-            }
-        )
-
-        # Check the patched object was updated and saved as expected
-        self.patched_obj.update_from_asset_data.assert_called_once_with(TEST_ASSET_DATA)
-        self.patched_obj.save.assert_called_once()
-
-    def test_minutes(self):
-        expected_datemodified = datetime.datetime.utcnow() - datetime.timedelta(
-            minutes=30
-        )
-        output = self.call_command(minutes=30)
-
-        # Check the command output
-        self.assertIn(
-            "Looking for video assets modified in the last 30 minute(s)", output
-        )
-
-        # Check the mocked API was called as expected
-        self.mock_api_client.asset_bank_client.media_list.assert_called_once_with(
-            {
-                "dateModified": expected_datemodified.strftime("%Y-%m-%dT%H:%M:%SZ"),
-                **self.common_api_kwargs,
-            }
-        )
-
-        # Check the patched object was updated and saved as expected
-        self.patched_obj.update_from_asset_data.assert_called_once_with(TEST_ASSET_DATA)
-        self.patched_obj.save.assert_called_once()
-
-    def test_hours(self):
-        expected_datemodified = datetime.datetime.utcnow() - datetime.timedelta(hours=3)
-        output = self.call_command(hours=3)
-
-        # Check the command output
-        self.assertIn("Looking for video assets modified in the last 3 hour(s)", output)
-
-        # Check the mocked API was called as expected
-        self.mock_api_client.asset_bank_client.media_list.assert_called_once_with(
-            {
-                "dateModified": expected_datemodified.strftime("%Y-%m-%dT%H:%M:%SZ"),
-                **self.common_api_kwargs,
-            }
-        )
-
-        # Check the patched object was updated and saved as expected
-        self.patched_obj.update_from_asset_data.assert_called_once_with(TEST_ASSET_DATA)
-        self.patched_obj.save.assert_called_once()
-
-    def test_days(self):
-        expected_datemodified = datetime.datetime.utcnow() - datetime.timedelta(days=3)
-        output = self.call_command(days=3)
-
-        # Check the command output
-        self.assertIn("Looking for video assets modified in the last 3 day(s)", output)
-
-        # Check the mocked API was called as expected
-        self.mock_api_client.asset_bank_client.media_list.assert_called_once_with(
-            {
-                "dateModified": expected_datemodified.strftime("%Y-%m-%dT%H:%M:%SZ"),
-                **self.common_api_kwargs,
-            }
-        )
-
-        # Check the patched object was updated and saved as expected
-        self.patched_obj.update_from_asset_data.assert_called_once_with(TEST_ASSET_DATA)
-        self.patched_obj.save.assert_called_once()
+    command_class = UpdateStaleVideos
+    uses_media_info_for_individual_assets = False
 
 
 class TestGetStaleObjects(TestCase):
