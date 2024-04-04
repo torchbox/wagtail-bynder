e30f70dedef15ac1e1eeedbcf2ba3a466c9b6a14
Andy Babic <andy.babic@torchbox.com>
2024-04-04 16:10:24 +0100

    Add 'help' to existing management commands

--- src/wagtail_bynder/management/commands/update_stale_documents.py
+++ src/wagtail_bynder/management/commands/update_stale_documents.py
@@ -1,9 +1,13 @@
+from django.utils.translation import gettext_lazy as _
 from wagtail.documents import get_document_model
 
 from .base import BaseBynderSyncCommand
 
 
 class Command(BaseBynderSyncCommand):
+    help = _(
+        "Update stale Wagtail document library items to reflect recent asset updates in Bynder."
+    )
     model = get_document_model()
     bynder_asset_type: str = "document"
     page_size: int = 200
