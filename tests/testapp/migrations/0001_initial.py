7051925a7e309b27cc8403eb8fe3a6a2dd691b8a
Andy Babic <andy.babic@torchbox.com>
2024-04-10 22:52:06 +0100

    Fix taggit migration dependency issue

--- tests/testapp/migrations/0001_initial.py
+++ tests/testapp/migrations/0001_initial.py
@@ -14,10 +14,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        (
-            "taggit",
-            "0006_rename_taggeditem_content_type_object_id_taggit_tagg_content_8fc721_idx",
-        ),
+        ("taggit", "0005_auto_20220424_2025"),
         ("wagtailcore", "0091_remove_revision_submitted_for_moderation"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
