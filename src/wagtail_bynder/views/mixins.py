0b75c9ef5259501169b7ab28c707b1b5717076a7
Andy Babic <andy.babic@torchbox.com>
2024-07-02 16:05:44 +0100

    Improve clash handling

--- src/wagtail_bynder/views/mixins.py
+++ src/wagtail_bynder/views/mixins.py
@@ -1,6 +1,7 @@
 from typing import TYPE_CHECKING
 
 from django.conf import settings
+from django.db import IntegrityError
 from django.shortcuts import redirect
 
 from wagtail_bynder.models import BynderAssetMixin
@@ -12,13 +13,33 @@ if TYPE_CHECKING:
 
 
 class BynderAssetCopyMixin:
+
+    model = type[BynderAssetMixin]
+
     def create_object(self, asset_id: str) -> BynderAssetMixin:
         client = get_bynder_client()
         obj: BynderAssetMixin = self.model(bynder_id=asset_id)
         data = client.asset_bank_client.media_info(asset_id)
         obj.update_from_asset_data(data)
-        obj.save()
-        return obj
+        try:
+            obj.save()
+        except IntegrityError as ie:
+            try:
+                # If the 'bynder_id' is already taken, find the existing object to return
+                existing = self.model.objects.get(bynder_id=asset_id)
+            except self.model.DoesNotExist:
+                # The error must relate to a different field, so throw the original error
+                raise ie from None
+            else:
+                # If the new file was copied to media storage, ensure it is deleted
+                try:
+                    if obj.file.path != existing.file.path:
+                        obj.file.delete()
+                except (ValueError, FileNotFoundError):
+                    pass
+                return existing
+        else:
+            return obj
 
     def update_object(self, asset_id: str, obj: BynderAssetMixin) -> BynderAssetMixin:
         client = get_bynder_client()
