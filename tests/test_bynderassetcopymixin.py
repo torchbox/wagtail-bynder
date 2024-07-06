11806c73960730d7c50f4352268be9c400be57f8
Andy Babic <andy.babic@torchbox.com>
2024-07-06 09:24:10 +0100

    Update tests

--- tests/test_bynderassetcopymixin.py
+++ tests/test_bynderassetcopymixin.py
@@ -2,8 +2,13 @@ from unittest import mock
 
 import responses
 
+from django.db import IntegrityError
+from django.http import HttpRequest
 from django.test import TestCase
-from wagtail.images import get_image_model
+from django.utils.functional import cached_property
+from django.views.generic.base import View
+from testapp.factories import CustomImageFactory
+from testapp.models import CustomImage
 from wagtail_factories import ImageFactory
 
 from wagtail_bynder.views.mixins import BynderAssetCopyMixin
@@ -17,8 +22,13 @@ TEST_ASSET_DATA = get_test_asset_data(id=TEST_ASSET_ID)
 class BynderAssetCopyMixinTests(TestCase):
     def setUp(self):
         super().setUp()
-        self.view = BynderAssetCopyMixin()
-        self.view.model = get_image_model()
+        request = HttpRequest()
+        request.method = ""
+        request.path = "/"
+
+        self.view = self.view_class()
+        self.view.setup(request)
+
         # Mock out Bynder API calls
         responses.add(
             responses.GET,
@@ -26,6 +36,20 @@ class BynderAssetCopyMixinTests(TestCase):
             json=TEST_ASSET_DATA,
             status=200,
         )
+
+    @cached_property
+    def view_class(self) -> type[BynderAssetCopyMixin]:
+        """
+        Use the mixin to create a functioning view class
+        that can be used in tests for this class.
+        """
+
+        class TestViewClass(BynderAssetCopyMixin, View):
+            """A basic view class utilising BynderAssetCopyMixin"""
+
+            model = CustomImage
+
+        return TestViewClass
 
     @responses.activate
     def test_create_object(self):
@@ -94,3 +118,74 @@ class BynderAssetCopyMixinTests(TestCase):
         is_up_to_date_mock.assert_called_once_with(TEST_ASSET_DATA)
         update_from_asset_data_mock.assert_called_once_with(TEST_ASSET_DATA)
         save_mock.assert_called_once()
+
+    @responses.activate
+    def test_create_object_clash_handling_after_update(self):
+        # Create an image with a matching bynder_id
+        existing = CustomImageFactory.create(bynder_id=TEST_ASSET_ID)
+
+        # Trigger the test target directly
+        with mock.patch.object(
+            self.view.model, "update_from_asset_data"
+        ) as update_from_asset_data_mock:
+            result = self.view.create_object(TEST_ASSET_ID)
+
+        # Check behavior and result
+        update_from_asset_data_mock.assert_called_once_with(TEST_ASSET_DATA)
+        self.assertEqual(result, existing)
+
+    @responses.activate
+    def test_create_object_clash_handling_on_save(self):
+        def create_dupe_and_throw_integrity_error():
+            CustomImageFactory.create(bynder_id=TEST_ASSET_ID)
+            raise IntegrityError("bynder_id must be unique")
+
+        fake_obj = mock.MagicMock()
+        fake_obj.save = create_dupe_and_throw_integrity_error
+
+        with (
+            # Patch update_from_asset_data() to prevent download attempts
+            # and other unnecessary work
+            mock.patch.object(
+                self.view.model,
+                "update_from_asset_data",
+            ),
+            # Patch build_object_from_data to return a mock with a patched save() method
+            mock.patch.object(
+                self.view,
+                "build_object_from_data",
+                return_value=fake_obj,
+            ),
+        ):
+            # The IntegrityError should be captured, and the object
+            # that was saved previously should be found and returned
+            result = self.view.create_object(TEST_ASSET_ID)
+
+        self.assertEqual(result.bynder_id, TEST_ASSET_ID)
+
+    @responses.activate
+    def test_create_object_clash_handling_on_save_when_bynder_id_match_not_found(self):
+        def throw_integrity_error():
+            raise IntegrityError("Some other field must be unique")
+
+        fake_obj = mock.MagicMock()
+        fake_obj.save = throw_integrity_error
+
+        with (
+            # Patch update_from_asset_data() to prevent download attempts
+            # and other unnecessary work
+            mock.patch.object(
+                self.view.model,
+                "update_from_asset_data",
+            ),
+            # Patch build_object_from_data to return a mock with a patched save() method
+            mock.patch.object(
+                self.view,
+                "build_object_from_data",
+                return_value=fake_obj,
+            ),
+            self.assertRaises(IntegrityError),
+        ):
+            # With no 'bynder_id' match to be found, the error is allowed
+            # to bubble up
+            self.view.create_object(TEST_ASSET_ID)
