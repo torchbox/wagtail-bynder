91dccd73dbfe07733976565e7a52e2b0858098d2
Andy Babic <andy.babic@torchbox.com>
2024-07-29 12:29:26 +0100

    Manually inject required chooser base URLs into the admin template

--- src/wagtail_bynder/templatetags/bynder_tags.py
+++ src/wagtail_bynder/templatetags/bynder_tags.py
@@ -1,5 +1,8 @@
 from django import template
 from django.conf import settings
+from django.urls import reverse
+
+from wagtail_bynder import get_video_model
 
 
 register = template.Library()
@@ -13,3 +16,30 @@ def bynder_domain():
 @register.simple_tag
 def bynder_compactview_api_token():
     return getattr(settings, "BYNDER_COMPACTVIEW_API_TOKEN", "")
+
+
+@register.simple_tag
+def get_image_chosen_base_url():
+    url = reverse("wagtailimages_chooser:choose")
+    if not url.endswith("/"):
+        url += "/"
+    return url
+
+
+@register.simple_tag
+def get_document_chosen_base_url():
+    url = reverse("wagtaildocs_chooser:choose")
+    if not url.endswith("/"):
+        url += "/"
+    return url
+
+
+@register.simple_tag
+def get_video_chosen_base_url():
+    model = get_video_model()
+    if model:
+        url = model.snippet_viewset.chooser_viewset.get_url_name("choose")
+        if not url.endswith("/"):
+            url += "/"
+        return url
+    return ""
